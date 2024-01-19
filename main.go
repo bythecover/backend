44d89328cdf56ff53ee90c55031c761ec2e60ed0
Grant Ralls <grantsralls@gmail.com>
2024-01-18 22:23:18 -0500

    adding option and vote page

--- main.go
+++ main.go
@@ -16,9 +16,6 @@ import (
 func SetupCors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "null")
-		// c.Header("Access-Control-Allow-Credentials", "true")
-		// c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		// c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
 	}
 }
 
