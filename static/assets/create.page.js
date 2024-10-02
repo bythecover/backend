976e9d006e0be338c8058332babcaf002bddfb94
Grant Ralls <grantsralls@gmail.com>
2024-10-01 21:07:34 -0400

    changing client side code

--- static/assets/create.page.js
+++ static/assets/create.page.js
@@ -1,9 +1,10 @@
-function addOption() {
-	const config = document.querySelector("[data-option-config]")
-	const newNode = config.cloneNode(true);
-	config.parentNode.appendChild(newNode)
-}
+const selector = document.querySelector("[data-option-config]").cloneNode();
 
+function addOption() {
+	const config = selector;
+	const newNode = config.cloneNode(true);
+	config.parentNode.appendChild(newNode);
+}
 
 function removeOption(e) {
 	if(document.querySelectorAll("[data-option-config]").length > 1) {
